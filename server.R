46c2bcf6b122958292b9c4b86d6a9bede5e1951d
Joseph Zhang <JZ.Zhang@outlook.com>
2018-03-07 02:59:04 -0800

    akfldsjggafalskdfjzxckjfa

--- server.R
+++ server.R
@@ -8,20 +8,8 @@ source('scripts/data_processing.R')
 
 
 server <- function(input, output) {
-<<<<<<< HEAD
-=======
-  output$bar <- renderPlot({
-    ggplot(#name of the generated table,
-            aes(#insert correct: x = , y = ) +
-            geom_bar(stat = 'identity') +
-            geom_text(aes(label = len), vjust = 1.6, color = 'white', size = 3.5) +
-            scale_fill_grey() +
-            coord_flip() +
-            xlab('Ratio of Degrees Given') +
-            ggtitle('Percentages of Different Degrees Given')
-            )
->>>>>>> 1a5763bdbf4d2efe4640c723b1304706bc4c7738
-    
+
+
   ree <- topFiveIncome(input$state1)  
     
   output$bar <- renderPlot(
@@ -29,6 +17,14 @@ server <- function(input, output) {
             coord_flip() + xlab('Ratio of Degrees Given') + ggtitle('Percentages of Different Degrees Given') +
             labs(title = "Top 5 Schools by Median Income in" + state1)
     )
-    
+        # ggplot(#name of the generated table,
+        #     aes(#insert correct: x = , y = ) +
+        #     geom_bar(stat = 'identity') +
+        #     geom_text(aes(label = len), vjust = 1.6, color = 'white', size = 3.5) +
+        #     scale_fill_grey() +
+        #     coord_flip() +
+        #     xlab('Ratio of Degrees Given') +
+        #     ggtitle('Percentages of Different Degrees Given')
+        #     )
   )  
 }
