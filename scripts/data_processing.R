72a256df9f85cff5a69d5a5760b6ee0cbdf21563
Abby Liu <hcliu@uw.edu>
2018-03-06 22:48:53 -0800

    Functions are ready

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -10,18 +10,35 @@ final_dataset <- left_join(scorecard_dataset, treasury_dataset) %>%
 
 #Rename columns headers
 new_col_names <- c('UnitID', 'State', 'Median_HH_Income', 'Computer_and_Information_Sciences', 
-                  'Education', 'Engineering', 'Humanities', 'Business', 'mean_earnings')
+                  'Education', 'Engineering', 'Humanities', 'Business', 'Mean_Student_Earnings')
 names(final_dataset) <- new_col_names
   
 #Takes in user-entered state code (unspecified capitalization) and retrieves the top 5 schools with
 #highest median hh income and mean student earnings.
-top5InState <- function() {
-  
+topFiveIncome <- function(user_state) {
+  user_state <- toupper(user_state)
+  top_5_income <- final_dataset %>%
+    filter(State == user_state) %>%
+    select(Median_HH_Income) %>%
+    arrange(Median_HH_Income) %>%
+    head(5)
+  return(top_5_income)
+}
+
+topFiveEarnings <- function(user_state) {
+  user_state <- toupper(user_state)
+  top_5_earnings <- final_dataset %>%
+    filter(State == user_state) %>%
+    select(Mean_Student_Earnings) %>%
+    arrange(Mean_Student_Earnings) %>%
+    head(5)
+  return(top_5_earnings)
 }
 
 #Takes in user-entered school name (unspecified capitalization) and retrieves information for the percent
 #of degrees awarded.
-percentDegreesAwarded <- function() {
-  
+percentDegreesAwarded <- function(user_entered_school) {
+  user_entered_school <- 
+    
 }
   
