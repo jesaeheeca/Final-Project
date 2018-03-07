18b1a0885adeb422e9e598aef45cbc544a64c889
Brandon Hong <brandonhong@D-173-250-148-209.dhcp4.washington.edu>
2018-03-07 02:34:29 -0800

    fix data_processing.R

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -1,47 +1,58 @@
 library('dplyr')
-scorecard_dataset <- read.csv(file = 'data/Most-Recent-Cohorts-Scorecard-Elements.csv')
-treasury_dataset <- read.csv(file = 'data/Most-Recent-Cohorts-Treasury-Elements.csv')
 
-#Join the two datasets, filter for only the columns we need
-final_dataset <- left_join(scorecard_dataset, treasury_dataset) %>%
-  filter(CURROPER == 1) %>% #only list schools that are currently operating
-  select(UNITID, STABBR, INSTNM, MEDIAN_HH_INC, PCIP11, PCIP13, PCIP14, PCIP24, PCIP52, MN_EARN_WNE_P10)
-
-#Rename columns headers
-new_col_names <- c('UnitID', 'State', 'School_Name', 'Median_HH_Income', 'Computer_and_Information_Sciences', 
-                  'Education', 'Engineering', 'Humanities', 'Business', 'Mean_Student_Earnings')
-names(final_dataset) <- new_col_names
-final_dataset$School_Name <- tolower(final_dataset$School_Name)
+getData <- function() { 
+  scorecard_dataset <- read.csv(file = 'data/Most-Recent-Cohorts-Scorecard-Elements.csv')
+  treasury_dataset <- read.csv(file = 'data/Most-Recent-Cohorts-Treasury-Elements.csv')
   
-#Takes in user-entered state code (unspecified capitalization) and retrieves the top 5 schools with the highest
-#median household income.
+  # Join the two datasets, filter for only the columns we need
+  final_dataset <- left_join(scorecard_dataset, treasury_dataset) %>%
+    filter(CURROPER == 1) %>% # only list schools that are currently operating
+    select(UNITID, STABBR, INSTNM, MEDIAN_HH_INC, PCIP11, PCIP13, PCIP14, PCIP24, PCIP52, MN_EARN_WNE_P10)
+  
+  # Rename columns headers
+  new_col_names <- c('UnitID', 'State', 'School_Name', 'Median_HH_Income', 'Computer_and_Information_Sciences', 
+                     'Education', 'Engineering', 'Humanities', 'Business', 'Mean_Student_Earnings')
+  names(final_dataset) <- new_col_names
+  final_dataset$School_Name <- tolower(final_dataset$School_Name)
+  final_dataset <- final_dataset[!grepl("NULL", final_dataset$Median_HH_Income),]
+  final_dataset <- final_dataset[!grepl("NULL", final_dataset$Mean_Student_Earnings),]
+  final_dataset <- final_dataset[!grepl("PrivacySuppressed", final_dataset$Median_HH_Income),]
+  final_dataset <- final_dataset[!grepl("PrivacySuppressed", final_dataset$Mean_Student_Earnings),]
+  return(final_dataset)
+}
+
+# Takes in user-entered state code (unspecified capitalization) and retrieves the top 5 schools with the highest
+# median household income.
 topFiveIncome <- function(user_state) {
+  data <- getData()
   user_state <- toupper(user_state)
-  top_5_income <- final_dataset %>%
+  top_5_income <- data %>%
     filter(State == user_state) %>%
-    select(Median_HH_Income) %>%
-    arrange(Median_HH_Income) %>%
+    select(Median_HH_Income, School_Name) %>%
+    arrange(desc(Median_HH_Income)) %>%
     head(5)
   return(top_5_income)
 }
 
-#Takes in user-entered state code (unspecified capitalization) and retrieves the top 5 schools with the highest
-#mean student earnings for independent students working and not enrolled 10 years after entry into the college.
+# Takes in user-entered state code (unspecified capitalization) and retrieves the top 5 schools with the highest
+# mean student earnings for independent students working and not enrolled 10 years after entry into the college.
 topFiveEarnings <- function(user_state) {
+  data <- getData()
   user_state <- toupper(user_state)
-  top_5_earnings <- final_dataset %>%
+  top_5_earnings <- data %>%
     filter(State == user_state) %>%
-    select(Mean_Student_Earnings) %>%
-    arrange(Mean_Student_Earnings) %>%
+    select(Mean_Student_Earnings, School_Name) %>%
+    arrange(desc(Mean_Student_Earnings)) %>%
     head(5)
   return(top_5_earnings)
 }
 
-#Takes in user-entered school name (unspecified capitalization) and retrieves information for the percent
-#of degrees awarded.
+# Takes in user-entered school name (unspecified capitalization) and retrieves information for the percent
+# of degrees awarded.
 percentDegreesAwarded <- function(user_entered_school) {
+  data <- getData()
   user_entered_school <- tolower(user_entered_school)
-  degree_percents <- final_dataset %>%
+  degree_percents <- data %>%
     filter(grepl(user_entered_school, ignore.case = TRUE, final_dataset$School_Name)) %>%
     select(Computer_and_Information_Sciences, Education, Engineering, Humanities, Business)
 }
