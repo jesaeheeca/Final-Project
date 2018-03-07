24b70a983c42e223163a271eff68dae974d6fe65
Joseph Zhang <JZ.Zhang@outlook.com>
2018-03-06 23:08:23 -0800

    change input reference name

--- ui.R
+++ ui.R
@@ -8,14 +8,14 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
       
-      textInput("state", label = h3("Which 5 schools have the highest median household income in the given state?"),
+      textInput("state1", label = h3("Which 5 schools have the highest median household income in the given state?"),
                 value = "Enter a state abbreviation here..."),
       
-      textInput("state", label = h3("Which 5 schools have highest mean earnings of independent students working 
+      textInput("state2", label = h3("Which 5 schools have highest mean earnings of independent students working 
                                     and not enrolled 10 years after entry in the given state?"), 
                 value = "Enter a state abbreviation here..."),
       
-      textInput("state", label = h3("What percent of degrees are awarded in the given school?"), 
+      textInput("school", label = h3("What percent of degrees are awarded in the given school?"), 
                 value = "Enter a school here...")
       
     ),
