6492992ea835b9bce3f0b8a35a03c82ca62d5ac2
Joseph Zhang <JZ.Zhang@outlook.com>
2018-03-06 22:46:48 -0800

    state _abbreviation_

--- ui.R
+++ ui.R
@@ -9,11 +9,11 @@ fluidPage(
     sidebarPanel(
       
       textInput("state", label = h3("Which 5 schools have the highest median household income in the given state?"),
-                value = "Enter a state here..."),
+                value = "Enter a state abbreviation here..."),
       
       textInput("state", label = h3("Which 5 schools have highest mean earnings of independent students working 
                                     and not enrolled 10 years after entry in the given state?"), 
-                value = "Enter a state here..."),
+                value = "Enter a state abbreviation here..."),
       
       textInput("state", label = h3("What percent of degrees are awarded in the given school?"), 
                 value = "Enter a school here...")
