a524fb009679a2a52c419e25a294ea6abe515892
Joseph Zhang <JZ.Zhang@outlook.com>
2018-03-07 02:37:09 -0800

    ggplot attempt

--- server.R
+++ server.R
@@ -1,4 +1,7 @@
 # server.R
+install.packages('ggplot2')
+library(ggplot2)
+
 function(input, output) {
   
   
@@ -9,3 +12,15 @@ function(input, output) {
   
   
 }
+
+
+
+
+
+
+source(./scripts/data_processing.R)
+
+ree <- topFiveIncome(state1)
+
+gee <- ggplot(ree, aes(x = "School_Name", y = "Median_HH_Income")) + geom_bar(stat = "identity") + 
+  labs(title = "Top 5 Schools by Median Income in" + state1)
